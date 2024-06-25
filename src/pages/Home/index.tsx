6176346a4b4c88de3d42d4191127479c968bef3e
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-06-25 20:19:00 +0530

    Add: changes on homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom"
 const Home = () => {
   return (
     <div className="h-[100vh] flex flex-col justify-center items-center">
-      <h1 className="text-3xl">This is HOME Page</h1>
+      <h1 className="text-lg font-semibold text-blue-600">THIS WEB APP IS ONLY FOR MOBILES</h1>
+      <h1 className="text-3xl">This is Home Page</h1>
       <Link to="/restaurant/anyrandomid">
       <h1 className="text-xl text-orange-500">
       Click here to view Menu <RightOutlined />
